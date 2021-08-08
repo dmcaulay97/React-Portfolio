234efb392ed361cb765b27bdfd838818b2d98c6c
Dylan McAulay <dmcaulay97@gmail.com>
2021-08-08 11:23:15 -0400

    adding initial files for github pages test

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Welcome from './components/Welcome'
+
+export default function App() {
+    return (
+        <div>
+            <Welcome />
+        </div>
+    );
+}
