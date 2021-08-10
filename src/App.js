c6211e760847e057fd67c794b514ee0b3ea45e93
Dylan McAulay <dmcaulay97@gmail.com>
2021-08-10 08:54:50 -0400

    added nav and footer and basic about page. Created basicstructure for mobile layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-// First we import our Hello component from our components folder.
-import Hello from './components/Hello';
+import React from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Footer from "./components/Footer"
 
-// React apps typically have a single App component at the very top that can reference other React components.
-// This component, `App`, is our main component that is importing `Hello` and rendering it in the return method.
 function App() {
-  return <Hello />;
+  return (
+
+    <div>
+      <Nav />
+      <About />
+      <Footer />
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="mt-auto">
+            <ul className="nav justify-content-center">
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Github</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">LinkedIn</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Third Thing</a>
+                </li>
+            </ul>
+        </footer>
+    );
+}
+
+export default Footer;
